6cc0f7eb24c717002e8c8f7465c7cfa4ff400702
paola <chiappettapaola@gmail.com>
2022-05-16 21:00:55 -0300

    Carpeta components + NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import './NavBar.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const NavBar = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Sigma HR - Consultora de Recursos Humanos
+              </Typography>
+              <Button color="inherit">Home</Button>
+              <Button color="inherit">Nosotros</Button>
+              <Button color="inherit">Servicios</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      );
+}
+
+export default NavBar
